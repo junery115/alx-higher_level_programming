7b3b521dc9bf1d2e190b3152ff4b03c0fc6696fd
Japheth Awah <junery115@gmail.com>
2022-12-02 02:52:59 +0100

    fixed function to not use sum

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+"""Print the addition of all arguments."""
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
     import sys
 
-    total = 0
+    tot = 0
     for i in range(len(sys.argv) - 1):
-        sum += int(sys.argv[i + 1])
-    print("{}".format(sum))
+        tot += int(sys.argv[i + 1])
+    print("{}".format(tot))
